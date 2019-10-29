07c747c6ca7a03549b2921c31173c6ab7beca9ff
Student <example@example.com>
2019-10-29 14:27:42 -0700

    inital commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("form#luck").submit(function(event) {
+    event.preventDefault();
+    $("#fortune").show();
+    $("input:checkbox[name=luck]:checked").each(function(){
+      var yourluck = $(this).val();
+        $('#fortune').append(yourluck + "<br>");
+      });
+        $("#luck").hide();
+    });
+});
